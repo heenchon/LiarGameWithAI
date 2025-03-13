ef7ca772babd607a86c1e2a3e9e22451d057c494
loenna <arterycutter@gmail.com>
2025-03-13 18:35:28 +0900

    삭제

---
+++ Source/LiarGameWithAI/Chair.cpp
@@ -0,0 +1,30 @@
+#include "Chair.h"
+
+AChair::AChair()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	RootMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Root"));
+	SetRootComponent(RootMesh);
+
+	ConstructorHelpers::FObjectFinder<UStaticMesh> tempMesh(TEXT("/Script/Engine.StaticMesh'/Game/StarterContent/Props/SM_Chair.SM_Chair'"));
+
+	if (tempMesh.Succeeded())
+	{
+		RootMesh->SetStaticMesh(tempMesh.Object);
+	}
+
+	SitPosition = CreateDefaultSubobject<USceneComponent>(TEXT("SitPosition"));
+	SitPosition->SetupAttachment(RootMesh);
+	SitPosition->SetRelativeLocation(FVector(20.f, 0.f, 60.f));
+}
+
+void AChair::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// Test...
+	FActorSpawnParameters Params;
+	GetWorld()->SpawnActor<ALiarGameWithAICharacter>(PlayerFactory, SitPosition->GetComponentTransform(), Params);
+}
+
